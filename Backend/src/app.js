c295843aaa07c215fa7727a38e30dc299e99f6ae
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2025-08-14 13:22:03 +0530

    Add admin and solve some bug

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,10 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow requests from any origin
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
